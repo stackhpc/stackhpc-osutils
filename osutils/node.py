9de5b163ba20bf674a9aa021447d4d8b8d7862a8
Bharat Kunwar <bharat@stackhpc.com>
2021-08-09 15:53:19 +0100

    Format code using black

--- osutils/node.py
+++ osutils/node.py
@@ -2,21 +2,22 @@ from __future__ import print_function
 from osutils.common import Connection
 import re
 
-class Node( object ):
-    def __init__( self ):
+
+class Node(object):
+    def __init__(self):
         self.client = Connection().client
 
     @classmethod
     def filter_by_server_name(cls, pattern, query):
         self = cls()
         pattern = re.compile(pattern)
-        query['instance_name'] = 'instance_info.display_name'
+        query["instance_name"] = "instance_info.display_name"
         _result = Node.get_metadata(query)
         result = []
         for r in _result:
-            instance_name = r['value'].pop('instance_name')
+            instance_name = r["value"].pop("instance_name")
             if instance_name and pattern.match(instance_name):
-                result.append(dict(key=instance_name, value=r['value']))
+                result.append(dict(key=instance_name, value=r["value"]))
         return result
 
     @classmethod
@@ -27,13 +28,13 @@ class Node( object ):
         for node in self.client.baremetal.nodes(details=True, **kwargs):
             _result = {}
             for k, q in query.items():
-                args = q.split('.')
+                args = q.split(".")
                 intermediate = node
                 for arg in args:
-                    if arg == '*':
+                    if arg == "*":
                         intermediate = intermediate.keys()
                         break
-                    elif arg == '':
+                    elif arg == "":
                         continue
                     else:
                         intermediate = intermediate.get(arg, {})
